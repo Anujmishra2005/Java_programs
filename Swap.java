b507542992f1b458c727c60efbf372c6bf302749
Anuj Mishra <anujmishraan2005@gmail.com>
2024-04-10 23:52:27 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,18 @@
+class Swap
+{
+public static void main(String[] args) 
+{
+  Pal();
+}
+static void Pal() 
+{
+int i = 1;
+while (i <= 10) 
+{
+    System.out.println("i = " + i);
+    i++;
+}
+
+System.out.println("----");
+}
+}
