9432bea7448ee21ffd3d80227b8913e4d54c3f61
Anuj Mishra <anujmishraan2005@gmail.com>
2024-04-10 23:51:48 +0530

    Add files via upload

---
+++ Gap.java
@@ -0,0 +1,17 @@
+class Gap 
+{
+public static void main(String[] args) 
+{
+   Wrap();
+}
+static void Wrap() 
+{
+for (int i = 0; i <= 4; i++) 
+{
+  for (int j = 0; j <= i; j++) 
+{
+   System.out.println("i =" + i + " j =" + j);
+ }
+}
+ }
+}
