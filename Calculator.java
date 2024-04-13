c4ae590bd8560571c468842b5315c8b4405aa5bd
Anuj Mishra <anujmishraan2005@gmail.com>
2024-04-13 23:56:14 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -22,7 +22,6 @@ class Main {
         System.out.println(number1 + " + " + number2 + " = " + result);
         break;
 
-      // performs subtraction between numbers
       case '-':
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
