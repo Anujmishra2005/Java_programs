cc9229d4d2e952237266d7ea2f28ffea950949e7
Anuj Mishra <anujmishraan2005@gmail.com>
2024-04-18 23:42:35 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee:
+    def _-init__(self, emp_name, emp_id, emp_salary, emp_company):
+        self.emp_name = emp_name
+        self.emp_id = emp_id
+        self.emp_salary = emp_salary
+        self.emp_company = emp_company
+
+    def getdetails(self):
+        return f"Name: {self.emp_name}, ID: {self.emp_id}, Salary: {self.emp_salary}, Company: {self.emp_company}"
+
+emp = Employee("Abhay Rana", 2346 , 50000, "TechCorp")
+print(emp.getdetails())
