c327ca9886d7ad65c3aca51a5d2355e3f8f3ed63
Anuj Mishra <anujmishraan2005@gmail.com>
2024-05-03 23:37:06 +0530

    Update MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class MatrixAddition {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter the number of rows and columns of the matrices:");
         int rows = scanner.nextInt();
         int cols = scanner.nextInt();
