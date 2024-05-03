63e464f3e5f45b8e9662a85e15973aad6fb1c158
Anuj Mishra <anujmishraan2005@gmail.com>
2024-05-03 23:38:19 +0530

    Update MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -27,7 +27,6 @@ public class MatrixAddition {
                 result[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
-
         System.out.println("Matrix Addition Result:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
