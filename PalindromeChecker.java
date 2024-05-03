e8107f9d0ca4ade004e72cf00ff99addd6ee7c7e
Anuj Mishra <anujmishraan2005@gmail.com>
2024-05-03 23:35:34 +0530

    Update PalindromeChecker.java

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -2,7 +2,6 @@ public class PalindromeChecker {
     public static boolean isPalindrome(int number) {
         int reversedNumber = 0;
         int originalNumber = number;
-
         while (number != 0) {
             int digit = number % 10;
             reversedNumber = reversedNumber * 10 + digit;
