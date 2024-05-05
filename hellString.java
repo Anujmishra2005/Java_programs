0b5fcb4d7a44897737278e0e2f53b26cff649041
Anuj Mishra <anujmishraan2005@gmail.com>
2024-05-05 23:58:41 +0530

    Update hellString.java

--- hellString.java
+++ hellString.java
@@ -18,8 +18,7 @@ public class hellString {
     public char[] toCharacterArray() {
         String concatenated = concatStrings();
         return concatenated.toCharArray();
-    }
-    
+    }    
     public boolean isEqual(String str1, String str2) {
         return str1.equals(str2);
     }
