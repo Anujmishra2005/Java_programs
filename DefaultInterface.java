ae3ba45c2453b5211f57f5be4376a7e917ec5bfe
Anuj Mishra <anujmishraan2005@gmail.com>
2024-06-05 23:48:34 +0530

    Create DefaultInterface.java

---
+++ DefaultInterface.java
@@ -0,0 +1,26 @@
+package Methods;
+
+public interface DefaultInterface {
+
+    static int getHorsePower(int rpm, int torque) {
+        return (rpm * torque) / 5252;
+    }
+
+    default String speedUp() {
+        return "The vehicle is speeding up.";
+    }
+
+    default String slowDown() {
+        return "The vehicle is slowing down.";
+    }
+
+    default String turnAlarmOn() {
+        return "The vehicle alarm is on.";
+    }
+
+    default String turnAlarmOff() {
+        return "The vehicle alarm is off.";
+    }
+    
+    String getBrand();
+}
