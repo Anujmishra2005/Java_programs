bb97342ec2c174a5d2c2e729621367c8c54c3190
Anuj Mishra <anujmishraan2005@gmail.com>
2024-06-10 00:00:45 +0530

    Update BufferedReaderClass.java

--- BufferedReaderClass.java
+++ BufferedReaderClass.java
@@ -3,7 +3,6 @@ package java_io_pkg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class BufferedReaderClass {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
